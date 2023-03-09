5f7417a3cdb3e4a3b3aa55b09f43f47bcf4e0ece
nicosis <nicosis@gmail.com>
2023-03-09 13:56:30 +0000

    first commit

--- countVowels-bonus.js
+++ countVowels-bonus.js
@@ -1,7 +1,23 @@
-function countVowels(word, anotherWord) {
+function countVowels(palabra, otraPalabra) {
 
-    // Happy coding
-}
+    let vocales = "aeiouAEIOUáéíóúÁÉÍÓÚ"
+    let contador = 0
+
+    for (let letra = 0; letra < palabra.length; letra++) {
+        if (vocales.indexOf(palabra[letra]) !== -1) {
+            contador++;
+        };
+    };
+
+    for (let letra2 = 0; letra2 < otraPalabra.length; letra2++) {
+        if (vocales.indexOf(otraPalabra[letra2]) !== -1) {
+            contador++;
+        };
+    };
+
+    return contador;
+
+};
 
 console.log(countVowels("calor", "sal")); // 3
 console.log(countVowels("la", "le")); // 2
--- countVowels.js
+++ countVowels.js
@@ -1,6 +1,17 @@
-function countVowels(word) {
-        // Happy coding
-}
+function countVowels(palabra) {
+
+        let vocales = "aeiouAEIOUáéíóúÁÉÍÓÚ"
+        let contador = 0
+
+        for (let letra = 0; letra < palabra.length; letra++) {
+                if (vocales.indexOf(palabra[letra]) !== -1) {
+                        contador++;
+                };
+        };
+
+        return contador;
+
+};
 
 console.log(countVowels("calor")); // 2
 console.log(countVowels("la")); // 1
