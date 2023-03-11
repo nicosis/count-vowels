53047908de739826a5181f1f8e28bb31c025d1c2
nicosis <nicosis@gmail.com>
2023-03-11 06:27:08 +0000

    bonus start

--- countVowels.js
+++ countVowels.js
@@ -19,5 +19,3 @@ function countVowels(palabra) {
 console.log(countVowels("calor")); // 2
 console.log(countVowels("la")); // 1
 console.log(countVowels("crwth")); // 0 https://en.wikipedia.org/wiki/Crwth
-
-let i = 0
